da98b2ce403cf65ca0b44216d7e419114ce6714e
Zach Newton <znewton@iastate.edu>
2018-10-21 14:40:25 -0500

    Sort keys alphabetically

--- recipes.js
+++ recipes.js
@@ -79,19 +79,70 @@ const recipes = {
     title: 'Tomato Bruschetta',
     name: 'bruschetta',
     description: '',
-    tags: [],
-    ingredients: [],
-    directions: [],
-    notes: [],
+    tags: ['appetizer', 'dip', 'fan favorite'],
+    ingredients: [
+      '5 medium sized Tomatoes (more firm than soft)',
+      '4 cloves Garlic',
+      '~ 1 cup fresh Basil',
+      'drizzle of Balsamic Vinaigrette',
+      'Salt',
+      '4+ oz Goat Cheese',
+      '1-2 baguettes',
+      'Olive Oil',
+    ],
+    directions: [
+      'Deseed and dice tomatoes; place in bowl, stir in a good amount of salt',
+      'Mince garlic; roughly chop basil into small ribbons',
+      'Mix balsamic, garlic, and basil into tomatoes; salt and balsamic to taste',
+      'Thinly slice baguette; place pieces onto baking sheet(s); brush with olive oil',
+      'Place bread pans into 400&deg; oven or under broiler until golden/brown around edges',
+      'Place goat cheese on a small plate with knives',
+      'Drain tomato juice from bruschetta; serve with spoons for scooping onto bread',
+    ],
+    notes: [
+      "Herbed goat cheese seems to be very popular when it's an option.",
+      "In case you've never had bruschetta: spread goat cheese on slice of baguette, scoop tomatoes on top of goat cheese, enjoy.",
+      'Optionally, reserve balsamic to drizzle over each piece instead of mixing it in',
+      'Optionally, serve with freshly grated parmigiano to sprinkle on top of each piece',
+    ],
   },
   thai_peanut_noodles: {
     title: 'Thai Peanut Noodles',
     name: 'thai_peanut_noodles',
-    description: '',
-    tags: [],
-    ingredients: [],
-    directions: [],
-    notes: [],
+    description:
+      'Have you ever thought, "Damn, I could go for a spicy, salty, peanut butter sandwich on noodles right now"? Either way, try this recipe. I have yet to find someone who doesn\'t love it.',
+    tags: ['vegan', 'pasta', 'leftovers', 'fan favorite'],
+    ingredients: [
+      '1/3 cup Chunky Peanut Butter',
+      '1/3 cup Soy Sauce',
+      '2 tbsp Honey',
+      '1 tbsp Sesame oil',
+      '2 tbsp Canola oil',
+      '1 tbsp Rice Vinegar',
+      '1 tsp Chili Garlic Hot Sauce',
+      '2 cloves Fresh Garlic',
+      '2 tsp Fresh Ginger',
+      '1-2 Green Onions',
+      '12 oz Spaghetti Noodles',
+      '4-5 Carrots',
+      '1-2 Red/Yellow/Orange Bell Peppers',
+      'Additional ginger, garlic, soy sauce and canola oil for stir frying',
+    ],
+    directions: [
+      'Finely mince garlic, ginger, and green onions',
+      'Whisk together Peanut Butter, Soy sauce, Honey, Sesame Oil, Canola Oil, Rice Vinegar, Red Pepper Flakes, Garlic, and Ginger',
+      "Cut carrots into sticks; deseed bell peppers and slice relatively thinly; just think about sizes you'd want to mix and eat on the same fork as noodles",
+      'Stir fry carrots and bell pepper in canola oil, soy sauce (, and some more minced ginger & garlic for extra flavor) in cast iron skillet',
+      'Cook spaghetti (in heavily salted water, duh)',
+      'Combine all in large bowl; for easiest mixing, pour in half of everything, then the next half',
+    ],
+    notes: [
+      'Stir fried veggies are technically optional, but no guarantees people will like it. These be uncharted waters',
+      'Honey can be replaced with just plain sugar',
+      'Red Pepper flakes can substitute for hot sauce. Whatever you like most!',
+      'Toasted Sesame Oil is also fantastic',
+      "Make a double batch. You'll thank yourself later.",
+    ],
   },
   pesto: {
     title: 'Pesto',
@@ -238,12 +289,43 @@ const recipes = {
   acorn_squash_pasta: {
     title: 'Acorn Squash Pasta',
     name: 'acorn_squash_pasta',
-    courtesy_of: 'Unnamed Workiva Cafe Chef',
-    description: '',
-    tags: [],
-    ingredients: [],
-    directions: [],
-    notes: [],
+    courtesy_of: 'Workiva Cafe Chef',
+    description:
+      "This dish was served at the Workiva Cafe for a week in the Fall 2016. Technically Workiva property, so please don't sell it",
+    tags: [ 'pasta', 'Fall' ],
+    ingredients: [
+      '2 Acorn Squash',
+      'Olive oil',
+      'Salt',
+      'Ground Cumin',
+      'Ground Nutmeg',
+      'Cinnamon',
+      'Cayenne',
+      'Ground All-spice',
+      'Black Pepper',
+      '16 oz Trofie noodles',
+      '4 oz Browned Butter',
+      'Several leaves Fresh Sage',
+      'Parmigiano Regiano for garnish',
+    ],
+    directions: [
+      'Skin and dice acorn squash; toss with Olive oil, salt, cumin, nutmeg, cinnamon, cayenne, all-spice, and pepper',
+      'Place on baking sheet(s) and into 375&deg; oven for 45-60min or until starting to brown around the edges',
+      'Roughly chop sage',
+      'Cook noodles until al dente; reserve some pasta water',
+      `In metal skillet: 
+        <ul>
+          <li>heat butter,</li>
+          <li>add some pasta water, reduce,</li>
+          <li>add sage, a bit more water, reduce</li>
+          <li>add noodles, touch more water, reduce</li>
+          <li>add squash</li>
+        </ul>`,
+      'Plate and serve with freshly grated parmesan on top'
+    ],
+    notes: [
+      'Squash and butter can be made and refrigerated well-beforehand.'
+    ],
   },
   roasted_veggie_hoagies: {
     title: 'Roasted Veggie & Mushroom Hoagies',
@@ -356,6 +438,32 @@ const recipes = {
     directions: [],
     notes: [],
   },
+  brazilian_fries: {
+    title: 'Brazilian Fries',
+    name: 'brazilian_fries',
+    description: 'Best made and eaten semi-buzzed at 1am, these fries will knock your drunk friends\' socks off.',
+    tags: ['appetizer', 'side', 'drunk-food', 'easy'],
+    ingredients: [
+      'Yukon Gold (or similar) potatoes',
+      'Salt',
+      'Pepper',
+      'Cayenne',
+      'Olive Oil',
+      'Mayonnaise',
+      'Garlic Powder'
+    ],
+    directions: [
+      'Cut potatoes into wedges',
+      'Toss with olive oil, salt, pepper, and cayenne',
+      'Place on baking sheet with a skinless-side touching the sheet',
+      'Bake at 420&deg; for ~30 min, flip each wedge (as well as possible, depending on amount of alcohol consumption and laziness); cook for another 15-30 min',
+      'Mix together mayonnaise, garlic powder, salt, pepper, and a touch of cayenne',
+      'Never risk Cafe Beaudelaire being out of fries again'
+    ],
+    notes: [
+      'Exercise caution when handling a knife while intoxicated'
+    ],
+  }
 };
 
 if (typeof window === 'undefined') module.exports = recipes;
